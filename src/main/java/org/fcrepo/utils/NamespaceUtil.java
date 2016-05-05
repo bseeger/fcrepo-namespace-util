2d243f25d106754779880a954f26c42842c71c87
bseeger <bseeger@amherst.edu>
2016-05-05 14:12:24 -0400

    Adds way to load prefixes from a file.

--- src/main/java/org/fcrepo/utils/NamespaceUtil.java
+++ src/main/java/org/fcrepo/utils/NamespaceUtil.java
@@ -20,11 +20,14 @@ import static org.fcrepo.kernel.modeshape.utils.NamespaceTools.getNamespaces;
 
 import java.util.Map;
 import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import javax.inject.Inject;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.jcr.NamespaceException;
+import javax.jcr.NamespaceRegistry;
 
 import org.fcrepo.http.commons.session.SessionFactory;
 
@@ -56,8 +59,11 @@ public class NamespaceUtil {
             final NamespaceUtil nsUtil = new NamespaceUtil();
             ctx = new ClassPathXmlApplicationContext("classpath:/spring/master.xml");
             ctx.getBeanFactory().autowireBeanProperties(nsUtil, AutowireCapableBeanFactory.AUTOWIRE_BY_TYPE, false);
-            nsUtil.run();
+            nsUtil.run(args.length > 0 ? args[0] : null);
         } catch (RepositoryException ex) {
+            ex.printStackTrace();
+        } catch (FileNotFoundException ex) {
+            System.out.println("RDF Prefix file not found.");
             ex.printStackTrace();
         } finally {
             if (null != ctx) {
@@ -69,19 +75,47 @@ public class NamespaceUtil {
     /**
      * Run the namespace change utility
      **/
-    public void run() throws RepositoryException {
+    public void run(final String prefixFilename) throws RepositoryException, FileNotFoundException {
         LOGGER.info("Starting namespace utility");
-        prompt(sessionFactory.getInternalSession());
+        final Session session = sessionFactory.getInternalSession();
+        if (prefixFilename != null) {
+          LOGGER.info("Importing prefixes from file: {}", prefixFilename);
+          loadPrefixFile(session, prefixFilename);
+        }
+        prompt(session);
         LOGGER.info("Stopping namespace utility");
+    }
+
+    private void loadPrefixFile(final Session session, final String prefixFile)
+                   throws RepositoryException, FileNotFoundException  {
+
+        final Scanner scan = new Scanner(new FileInputStream(prefixFile));
+        final NamespaceRegistry registry = session.getWorkspace().getNamespaceRegistry();
+
+        System.out.println("#################### Importing Prefixes #########################");
+
+        while (scan.hasNextLine()) {
+            final String[] split = scan.nextLine().split(":", 2);
+            final String prefix = split[0].trim();
+            final String value = split[1].trim();
+
+            System.out.println("Adding prefix " + prefix + ": " + value);
+            try {
+                registry.registerNamespace(prefix, value);
+                session.save();
+            } catch (final NamespaceException ex) {
+                System.out.println("Could not create prefix (" + prefix + "): " + ex.getMessage());
+            }
+        }
     }
 
     private void prompt(final Session session) throws RepositoryException {
         final Map<String, String> namespaces = getNamespaces(session);
 
         System.out.println();
-        System.out.println("#####################################");
+        System.out.println("############### Current Fedora Prefix List ######################");
         namespaces.forEach((k, v) -> System.out.println("Prefix " + k + ": " + v));
-        System.out.println("#####################################");
+        System.out.println("#################################################################");
         System.out.println();
         System.out.println("Enter a prefix to change (or 'ctrl-d' to end):");
 
